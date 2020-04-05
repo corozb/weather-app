a84f5df4a51e38a39d4c9316bd6fa6b350c77949
Cristian Orozco <cristianorozco1984@gmail.com>
2020-04-05 14:27:09 -0500

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Logo from './components/Logo/Logo';
 import Forecast from './components/Forecast/Forecast';
 
 const App = () => {
 	return (
 		<div className='App'>
+			<header className='App-header'>
+				<Logo />
+				<h1>React Weather App</h1>
+			</header>
 			<main>
 				<Forecast />
 			</main>
