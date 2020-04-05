5f76fc187df7a8336b4f07644aebf0ff40517dc2
Cristian Orozco <cristianorozco1984@gmail.com>
2020-04-04 20:04:08 -0500

    conditions

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
+import Conditions from './Conditions/Conditions';
 
 const Forecast = () => {
-	let [responseObj, setResponseObj] = useState({});
+	let [city, setCity] = useState('medellin');
+	let [unit, setUnit] = useState('metric');
+	let [query, setQuery] = useState({});
+
+	const uriEncodeCity = encodeURIComponent(city);
 
 	function getForescast() {
 		const API_KEY = '525832fa65bb21b21a82519d028a5aac';
-		const city = 'medellin';
-		const URL_API = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+		// const city = 'medellin';
+		const URL_API = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${API_KEY}`;
 		fetch(URL_API)
 			.then((response) => response.json())
 			.then((response) => {
-				setResponseObj(response);
+				setQuery(response);
 			});
 	}
 
 	return (
 		<div>
 			<h2>Find Current Weather Conditions</h2>
-			<div>{JSON.stringify(responseObj)}</div>
 			<button onClick={getForescast}>Get Forecast</button>
+			<Conditions search={query} />
 		</div>
 	);
 };
